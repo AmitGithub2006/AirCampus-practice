f75f9dc73f252e1333f875103d5d1ffec7c7f50e
Amit Kumar <shakya94ak@gmail.com>
2022-06-21 23:36:00 +0530

    added 21st june practice

---
+++ 21June 2022/index.js
@@ -0,0 +1,57 @@
+// Q:- Check whether the given number is odd or even using loop,arithmetic operator and statement.
+// let n = parseInt(process.argv[2])
+// for (let i=0; i<=n; i++) {
+//     if (x % 2 == 0) {
+//             console.log("number is even");
+//     }
+//     else {
+//             console.log("number is odd");
+//     }
+// }
+
+// Q:-Using switch case, print different numbers for week days.
+// switch (day) {
+//   case 0:
+//     console.log("Monday");
+//     break;
+//   case 1:
+//     console.log("Tuesday");
+//     break;
+//   case 2:
+//     console.log("Wednesday");
+//     break;
+//   case 3:
+//     console.log("Thursday");
+//     break;
+//   case 4:
+//     console.log("Friday");
+//     break;
+//   case 5:
+//     console.log("Saturday");
+//     break;
+//   case 6:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// Q:-Take 3 numbers and find and return biggest of them. If all three numbers are same than print "None of them is biggest"
+// let key = true;
+//   switch (key) {
+//     case num1 > num2 && num1 > num3:
+//       return num1;
+//       break;
+
+//     case num2 > num1 && num2 > num3:
+//       return num2;
+//       break;
+
+//     case num3 > num1 && num3 > num2:
+//       return num3;
+//       break;
+
+//     default:
+//       return ("None of them is biggest");
+//       break;
+//   }
