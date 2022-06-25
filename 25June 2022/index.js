3a7ce097b9842872f742579ac83f3e6680074d65
Amit Kumar <shakya94ak@gmail.com>
2022-06-25 22:37:04 +0530

    25th june practice

---
+++ 25June 2022/index.js
@@ -0,0 +1,48 @@
+// 1.Print all the numbers in the given range.
+// function num(num1, num2) {
+//   if (num1 <= num2) {
+//     console.log(num1);
+//     return num(num1 + 1, num2);
+//   }
+// }
+// num(41, 50);
+
+//2.Print all the numbers in descending order in the given range.
+// function num(num1, num2) {
+//   if (num1 <= num2) {
+//     console.log(num2);
+//     return num(num1, num2 - 1);
+//   }
+// }
+// num(41, 50);
+
+//3.Print all the even numbers in the given range.
+// function num(num1, num2) {
+//   if (num1 <= num2) {
+//     if (num1 % 2 == 0) {
+//       console.log(num1);
+//     }
+//     return num(num1 + 1, num2);
+//   }
+// }
+// num(41, 50);
+
+//4.Find the factorial of a given number n.
+// function factorial(n,fact) {
+//     if(n>0){
+//         fact*=n;
+//         return factorial(n-1,fact);
+//     }
+//     return fact;
+// }
+// console.log(factorial(6,1));
+
+//5.Find the nth digit of the fibonacci series.
+// function fibo(a,b,counter){
+//     if(counter<=12){
+//         let c = a+b;
+//         return fibo(b,c,counter+1);
+//     }
+//     console.log(a);
+// }
+// fibo(0,1,1);
---
+++ 25June 2022/readme.md
@@ -0,0 +1,5 @@
+<img src= "ques1.png">
+<img src= "ques2.png">
+<img src= "ques3.png">
+<img src= "ques4.png">
+<img src= "ques5.png">
