e45bd66bd78ef02d7c7163fafd0115310cf32255
Amit Kumar <shakya94ak@gmail.com>
2022-06-28 23:49:37 +0530

    28th june practice added

---
+++ 28June 2022/index.js
@@ -0,0 +1,30 @@
+//1.Print this pattern.
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+// let num = 5;
+// for(let i = 1; i <= num; i++){
+//   stars = '';
+//   for(let j = 1; j <= i; j++){
+//     stars+= i;
+//   }
+//   console.log(stars);
+// }
+
+//2.Print this pattern.
+// *****
+// *****
+// *****
+// *****
+
+// let rows = 4, cols = 5;
+// for(let i = 1; i <= rows; i++){
+//   let stars = ''
+//   for(let j = 1; j <= cols; j++){
+//     stars+='*'
+//   }
+//   console.log(stars);
+// }
