f783de2f08826e6fd99d346d0f0b13d5225917dc
Amit Kumar <shakya94ak@gmail.com>
2022-07-05 17:00:00 +0530

    5th july practice

---
+++ 05July 2022/index.js
@@ -0,0 +1,48 @@
+//Primitive Data types
+
+//1. String
+// let str = "amit";
+// console.log(typeof str);
+
+// 2. Numbers
+// let num = 34.5;
+// console.log(typeof num);
+
+// 3. Boolean
+// let isGood = true;
+// console.log(typeof isGood);
+
+// 4. Null
+// let nullVar = null;
+// console.log(typeof nullVar);
+
+// 5. Undefined
+// let undef = undefined;
+// console.log(typeof undef);
+
+// 6. Symbol
+// let sym = Symbol('Hey');
+// console.log(typeof sym);
+
+//Reference Data Types
+
+// 1. Array
+let arr = [1,23,'amit', true,null, undefined]
+console.log(typeof arr);
+
+// 2. Object literals
+let marks = {
+    amit : 80,
+    sir : 90
+}
+console.log(typeof marks);
+
+//3. Functions
+function findName(){
+
+}
+console.log(typeof findName);
+
+// 4. Dates
+let date = new Date()
+console.log(typeof date);
---
+++ 05July 2022/readme.md
@@ -0,0 +1,2 @@
+<img src= "img1.png">
+<img src= "img2.png">
