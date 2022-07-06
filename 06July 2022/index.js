36607e11c26939921a7e790e9d179337534a0d78
Amit Kumar <shakya94ak@gmail.com>
2022-07-06 23:59:02 +0530

    6th july practice

---
+++ 06July 2022/index.js
@@ -0,0 +1,8 @@
+// Join array
+
+function arrayConcat(arr1, arr2){
+    // let arr3 = [].concat(arr1,arr2) OR
+      let arr3 = arr1.concat(arr2)
+      return arr3;
+    }
+    console.log(arrayConcat([1,3] , [2,5]))
