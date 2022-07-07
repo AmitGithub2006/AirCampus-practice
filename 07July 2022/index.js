3df5f39398eb55b4316ebb3f0f80ddbac5e9c7c4
Amit Kumar <shakya94ak@gmail.com>
2022-07-07 15:20:58 +0530

    7th july practice

---
+++ 07July 2022/index.js
@@ -0,0 +1,9 @@
+//1.Return 4th element of the array
+
+// let vehicles = ['Bike' , 'Scooter', 'Car', 'Truck', 'JCB','Helicopter']
+// console.log(vehicles[3]);
+
+//2.You are given an array arr, if the length of the array is greater than 5 return “Hello” else return “World”
+let arr = [1, 2, 3,4,5,6];
+if (arr.length > 5)  console.log("Hello");
+else console.log("World");
