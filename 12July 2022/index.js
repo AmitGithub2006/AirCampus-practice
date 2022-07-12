1b683e1040d9983e9328db0017221ebc6f60bfcf
Amit Kumar <shakya94ak@gmail.com>
2022-07-12 22:24:37 +0530

    12th july practice

---
+++ 12July 2022/index.js
@@ -0,0 +1,15 @@
+//1.Amit goes to a restaurant, but after seeing the huge menu. He was confused about what to order. Amit wanted to have something which has Paneer and does not have Rice. Help Amit choose a food item.(Give output without array)
+
+// let menu = ['Paneer Fried Rice', 'Chicken Biryani', 'Paneer Paratha', 'Cold Coffee']
+// let newArr = menu.find(function (veg){
+//     return veg.includes('Paneer') && !veg.includes('Rice');
+// })
+// console.log(newArr)
+
+//2.Amit goes to a restaurant, but after seeing the huge menu. He was confused about what to order. Amit wanted to have something which has Paneer and does not have Rice. Help Amit choose a food item.(Give output inside array)
+
+let menu = ['Paneer Butter Masala', 'Paneer Fried Rice', 'Chicken Biryani', 'Cold Coffee', 'Paneer Kofta']
+let newArr = menu.filter(function(veg){
+    return veg.includes('Paneer') && !veg.includes('Rice');
+})
+console.log(newArr)
