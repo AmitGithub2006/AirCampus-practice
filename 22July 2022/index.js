17f30031b813f00a78ffc1dac2537137e0b0715e
Amit Kumar <shakya94ak@gmail.com>
2022-07-22 18:27:05 +0530

    22nd july practice

---
+++ 22July 2022/index.js
@@ -0,0 +1,9 @@
+//1. Return the string in reverse.
+// let str = 'amit';
+// str = str.split('').reverse().join('');
+// console.log(str);
+
+//2. Join string1 and string2 separated by string3. Return the final string.
+let string1 = 'amit', string2 = 'kumar', string3 = '-';
+string1 = string1.concat(string3).concat(string2);
+console.log(string1);
