8a048fd02d873b47580c62fcc39ca4f8648441d7
Amit Kumar <shakya94ak@gmail.com>
2022-07-28 22:36:31 +0530

    28th july practice

---
+++ 28July 2022/index.js
@@ -0,0 +1,8 @@
+// document.querySelector("h1"); // it will select only the first h1.
+// document.querySelector("h1").innerHTML = "";
+
+// To select the second h1, we need to give class or id to it.
+// document.querySelector(".second").innerText = "second h1";
+
+// document.querySelector("li").innerText = "why are you outside";
+// document.querySelector("ul li").innerText = "i am first";
