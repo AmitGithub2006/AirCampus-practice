f8590954eed644383c049cbb1beab72d7a348bd5
Amit Kumar <shakya94ak@gmail.com>
2022-07-29 20:42:50 +0530

    29th july practice

---
+++ 29July 2022/index.js
@@ -0,0 +1,10 @@
+
+// document.querySelectorAll("a")
+// document.querySelector("a").getAttribute("href");
+// document.querySelector("a").setAttribute("href", "https://twitter.com");
+// document.querySelectorAll("a")[2]
+
+// document.querySelector("a").classList; // classList is used to get the element by its class.
+// document.querySelector("a").classList.add("visibility"); // classname 'visibility'  is added. It can be used to give css properties separately.
+// document.querySelector("a").classList.remove("visibility"); // classname 'visibility' is removed.
+// document.querySelector("a").classList.toggle("invisible"); // it will add the classname 'invisible' if not present or vice versa.
