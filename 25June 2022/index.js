f5dee5475541a347b5eb4988ad5555c226838649
AmitGithub2006 <105305969+AmitGithub2006@users.noreply.github.com>
2022-11-01 22:31:47 +0530

    practice

--- 25June 2022/index.js
+++ 25June 2022/index.js
@@ -28,21 +28,21 @@
 // num(41, 50);
 
 //4.Find the factorial of a given number n.
-// function factorial(n,fact) {
-//     if(n>0){
-//         fact*=n;
-//         return factorial(n-1,fact);
-//     }
-//     return fact;
-// }
-// console.log(factorial(6,1));
+function factorial(n,fact) {
+    if(n>0){
+        fact*=n;
+        return factorial(n-1,fact);
+    }
+    return fact;
+}
+console.log(factorial(6,1));
 
-//5.Find the nth digit of the fibonacci series.
-// function fibo(a,b,counter){
-//     if(counter<=12){
-//         let c = a+b;
-//         return fibo(b,c,counter+1);
-//     }
-//     console.log(a);
-// }
-// fibo(0,1,1);
+// 5.Find the nth digit of the fibonacci series.
+function fibo(a,b,counter){
+    if(counter<=12){
+        let c = a+b;
+        return fibo(b,c,counter+1);
+    }
+    console.log(a);
+}
+fibo(0,1,1);
